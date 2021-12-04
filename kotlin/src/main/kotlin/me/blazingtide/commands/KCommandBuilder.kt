40a9294a6496ddcd7ade024b752044153bdc02f7
BlazingTide <45052821+BlazingTide@users.noreply.github.com>
2021-12-04 13:01:24 -0800

    Remove redundancy

--- kotlin/src/main/kotlin/me/blazingtide/commands/KCommandBuilder.kt
+++ kotlin/src/main/kotlin/me/blazingtide/commands/KCommandBuilder.kt
@@ -22,10 +22,6 @@ class KCommandBuilder {
         this._usage = str
     }
 
-    fun execute(executor: (args: CommandArguments) -> Unit) {
-        this._executor = executor
-    }
-
     fun async(executor: (args: CommandArguments) -> Unit) {
         this._executor = executor
         this._async = true
