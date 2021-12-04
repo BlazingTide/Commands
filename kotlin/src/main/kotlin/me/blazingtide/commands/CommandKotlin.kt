bcfff9afb140f2555bb7fdd3c02dafe21eb74183
BlazingTide <45052821+BlazingTide@users.noreply.github.com>
2021-12-04 13:16:54 -0800

    Implement null-safety

--- kotlin/src/main/kotlin/me/blazingtide/commands/CommandKotlin.kt
+++ kotlin/src/main/kotlin/me/blazingtide/commands/CommandKotlin.kt
@@ -7,8 +7,14 @@ inline fun command(builder: KCommandBuilder.() -> Unit): Command {
 
     val builder = Commands.begin()
         .label(conf._label)
-        .permission(conf._permission)
-        .usage(conf._usage)
+
+    if (conf._permission != null) {
+        builder.permission(conf._permission)
+    }
+
+    if (conf._usage != null) {
+        builder.usage(conf._usage)
+    }
 
     if (conf._async) {
         builder.async()
